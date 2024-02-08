95ccf96602256486a14b7f8498785014928efbcd
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2024-02-08 12:29:14 +0530

    Update variables.tf

--- modules/BigQuery/variables.tf
+++ modules/BigQuery/variables.tf
@@ -4,7 +4,4 @@ variable "BigQ-DS-description" {}
 variable "BigQ-DS-location" {}
 variable "BigQ-DS-default_table_expiration_ms" {}
 variable "BigQ-DS-project" {}
-variable "BigQ-Table-table_id" {}
-variable "BigQ-Table-TP-type" {}
-variable "BigQ-deletion_protection" {}
-variable "schema" {}
+
